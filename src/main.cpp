4896d2a831ad2fc8cad27d23db89e79e876af62d
Dennis <denniscastrillon@gmail.com>
2021-04-08 14:00:11 -0500

    Se agrego los entornos del codigo a git 	new file:   include/README 	new file:   lib/README 	new file:   platformio.ini 	new file:   src/main.cpp 	new file:   test/README

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+#include <sensors/analog_sensor.h>
+#include <sensors/digital_sensor.h>
+#include <sensors/lm35_sensors.h>
+#include <processors/transformer.h>
+#include <processors/agreggator.h>
+#include <alarm/alarm.h>
+#include <output/printer.h>
+#include <actuator/led.h>
+
+uint8_t source_lm35 = A0;
+AnalogSensor sensor = AnalogSensor(0x7E, source_lm35);
+Lm35Sensors sensor_lm35 = Lm35Sensors(13, source_lm35);
+PromedioTotal<int> Suma_valores = PromedioTotal<int>();
+Led led = Led(0xAC, 13); 
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+    sensor.excecute();
+    sensor_lm35.excecute();
+    int raw_lm35= sensor_lm35.getValue().getValue();
+    int raw_read = sensor.getValue().getValue();
+    Serial.print(raw_read);
+    Serial.print("\n  ");
+    Serial.print(raw_lm35);
+    Serial.print("\n  ");
+
+    float temp_farenheit=To_Farenheit(raw_lm35);
+    Printer(Serial, temp_farenheit, "La temperatura en Farenheit fue: ");
+
+    float promedio = Suma_valores.Suma(raw_lm35);
+    bool compare = Compare(promedio);
+    WriteABC<bool> value = WriteABC<bool>(compare,source_lm35);
+    led.setValue(value);
+    led.excecute();
+  
+    Serial.print("Promedio: ");
+    Serial.print(promedio);
+    Serial.print("\n  ");
+    delay(5000);
+
+    Printer(Serial, promedio, "Promedio: ");
+  
+    
+  
+}
